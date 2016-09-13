78e143e5a83d688af60e0b01c9c875ea7c851927
plok74122 <plok74122@gmail.com>
2016-09-13 11:04:28 +0800

    api sign in , add authenticate token in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  scope :path => '/api/v1/', :module => "api_v1", :as => 'v1', :defaults => { :format => :json } do
+    post "login" => "auth#login"
+    post "logout" => "auth#logout"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
